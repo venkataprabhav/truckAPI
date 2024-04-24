2da9fd4028285d2c300dcb1a69d2fba09f6b9463
Venkata Akkenapragada <123014399+venkataprabhav@users.noreply.github.com>
2024-04-24 07:04:40 +0100

    Add files via upload

---
+++ Interview_Task_TruckAPI/Controllers/TruckController.cs
@@ -0,0 +1,51 @@
+﻿using Interview_Task_TruckAPI.Data;
+using Interview_Task_TruckAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Interview_Task_TruckAPI.Controllers
+{
+    [ApiController]             // This attribute indicates that this Controller handles HTTP API requests
+    [Route("[controller]")]     // This attribute specifies the base route for this Controller based on the controller's name
+    public class TruckController : Controller
+    {
+        // Declaration of Dependency, field to hold database context
+        private readonly ApplicationDbContext _context;     
+
+        // Constructor with Dependency Injection, injecting ApplicationDbContext into the Controller
+        public TruckController(ApplicationDbContext context)
+        {
+            // Dependency assigned to _context
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Truck>>> GetTrucks()
+        {
+            // Retrieves information of all trucks from the Trucks database
+            return await _context.Trucks.ToListAsync();
+        }
+
+        // HTTP POST request for /Truck
+        [HttpPost]
+        public async Task<ActionResult<Truck>> PostTruck([FromBody] Truck truck)
+        {
+            if (truck == null)
+                return BadRequest("Truck data is required");    // Returns BadRequest if no data for Truck is entered
+
+            // Utilization of Injected Dependency, Adds new Truck to the Truck table in the database
+            _context.Trucks.Add(truck);
+
+            // Save changes to the database
+            await _context.SaveChangesAsync();
+
+            // Return the created truck and its information data
+            return CreatedAtAction(nameof(GetTrucks), new { id = truck.Id }, truck);
+        }
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+    }
+}
---
+++ Interview_Task_TruckAPI/Models/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interview_Task_TruckAPI.Models
+{
+    // Represents the structure of a Truck 
+    public class Truck
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid(); 
+        public string? Registration { get; set; }
+        public decimal GrossWeight { get; set; }
+        public decimal TareWeight { get; set; }
+        public decimal NettWeight { get; set; }
+        public string? Haulier { get; set; }
+    }
+}
