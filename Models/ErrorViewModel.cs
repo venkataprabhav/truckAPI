96daabbda67bc80224c03eeb5c08dbc86baf17f7
Venkata Akkenapragada <123014399+venkataprabhav@users.noreply.github.com>
2024-04-24 07:00:49 +0100

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Interview_Task_TruckAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
